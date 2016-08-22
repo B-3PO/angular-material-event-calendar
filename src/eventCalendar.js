a3a21303b672bc97d598d454a1ab05b7759c4033
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2016-08-22 13:35:26 -0500

    small style updates move thumbing config to main directive module

--- src/eventCalendar.js
+++ src/eventCalendar.js
@@ -6,4 +6,15 @@
  * Calendar Component
  */
 angular
-  .module('material.components.eventCalendar', []);
+  .module('material.components.eventCalendar', [])
+  .config(addEventCalendarTheme);
+
+
+/*@ngInject*/
+function addEventCalendarTheme($injector, EVENT_CALENDAR_THEME) {
+  var $mdThemingProvider;
+  if ($injector.has('$mdThemingProvider')) {
+    $mdThemingProvider = $injector.get('$mdThemingProvider');
+    $mdThemingProvider.registerStyles(EVENT_CALENDAR_THEME);
+  }
+}
