b34d98218d390197fa44fd05a8246537d377509b
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2016-08-23 11:32:13 -0500

    pollyfill $$rAF throttle if ngMaterial is not loaded

--- src/eventCalendar.js
+++ src/eventCalendar.js
@@ -11,10 +11,32 @@ angular
 
 
 /*@ngInject*/
-function addEventCalendarTheme($injector, EVENT_CALENDAR_THEME) {
+function addEventCalendarTheme($injector, $provide, EVENT_CALENDAR_THEME) {
   var $mdThemingProvider;
   if ($injector.has('$mdThemingProvider')) {
     $mdThemingProvider = $injector.get('$mdThemingProvider');
     $mdThemingProvider.registerStyles(EVENT_CALENDAR_THEME);
+  } else {
+    $provide.decorator('$$rAF', ["$delegate", rAFDecorator])
   }
+}
+
+
+function rAFDecorator($delegate) {
+  $delegate.throttle = function(cb) {
+    var queuedArgs, alreadyQueued, queueCb, context;
+    return function debounced() {
+      queuedArgs = arguments;
+      context = this;
+      queueCb = cb;
+      if (!alreadyQueued) {
+        alreadyQueued = true;
+        $delegate(function() {
+          queueCb.apply(context, Array.prototype.slice.call(queuedArgs));
+          alreadyQueued = false;
+        });
+      }
+    };
+  };
+  return $delegate;
 }
