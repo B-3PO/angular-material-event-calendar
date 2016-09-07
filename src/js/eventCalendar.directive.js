808ed2fc04aab2de42445c37e39f408aa4d9cf27
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2016-09-06 23:08:26 -0500

    added create click to months day cell added `md-create-click` attribute added `md-show-create-link` attribute added `md-create-disabled` attribute added today button directive fixed incorrect event display on last day of week refactored some code minor style updates

--- src/js/eventCalendar.directive.js
+++ src/js/eventCalendar.directive.js
@@ -16,7 +16,7 @@ function eventCalendarDirective($injector, $parse) {
     restrict: 'E',
     require: ['mdEventCalendar', '?ngModel'],
     scope: {
-      events: '=mdEvents',
+      events: '=mdEvents'
     },
     compile: compile,
     controller: controller,
@@ -27,10 +27,13 @@ function eventCalendarDirective($injector, $parse) {
 
 
   function compile(tElement, tAttr) {
-    var fn = tAttr.mdEventClick ? $parse(tAttr.mdEventClick, null, true) : undefined;
+    var eventClickFunc = tAttr.mdEventClick ? $parse(tAttr.mdEventClick, null, true) : undefined;
+    var createEventClickFunc = tAttr.mdCreateEventClick ? $parse(tAttr.mdCreateEventClick, null, true) : undefined;
+    var mdCreateDisabled = tAttr.mdCreateDisabled ? $parse(tAttr.mdCreateDisabled) : undefined;
     tElement.append('<md-event-calendar-month></md-event-calendar-month>');
 
     return function postLink(scope, element, attrs, ctrls) {
+      var createDisabled = false;
       var mdEventCalendarCtrl = ctrls[0];
       var ngModelCtrl = ctrls[1];
       if ($mdTheming) {
@@ -38,10 +41,9 @@ function eventCalendarDirective($injector, $parse) {
         $mdTheming(element);
       }
 
-      mdEventCalendarCtrl.callEventClick = function (e, eventItem) {
-        if (!attrs.mdEventClick) { return; }
-        fn(scope.$parent, {$event: e, $selectedEvent: eventItem});
-      };
+      mdEventCalendarCtrl.isCreateDisabled = isCreateDisabled;
+      mdEventCalendarCtrl.callEventClick = callEventClick;
+      mdEventCalendarCtrl.createEventClick = createEventClick;
 
       if (ngModelCtrl) {
         ngModelCtrl.$render = render;
@@ -52,6 +54,34 @@ function eventCalendarDirective($injector, $parse) {
       function render() {
         var viewValue = ngModelCtrl.$viewValue || ngModelCtrl.$modelValue || [];
         mdEventCalendarCtrl.selectedEvents = [].concat(viewValue);
+      }
+
+
+      function callEventClick(e, eventItem) {
+        if (!attrs.mdEventClick) { return; }
+        eventClickFunc(scope.$parent, {$event: e, $selectedEvent: eventItem});
+      }
+
+      function createEventClick(e, date) {
+        if (!attrs.mdCreateEventClick) { return; }
+        createEventClickFunc(scope.$parent, {$event: e, $date: date});
+      }
+
+      function isCreateDisabled() {
+        return createDisabled;
+      }
+
+      // watch for create being disabled
+      if (mdCreateDisabled) {
+        scope.$watch(function () { return mdCreateDisabled(scope.$parent); }, function (value) {
+          createDisabled = value;
+          element.toggleClass('md-create-disabled', value);
+        });
+
+      // if no string was given check to see if the attr exists
+      } else if (tAttr.mdCreateDisabled !== undefined) {
+        createDisabled = true;
+        element.addClass('md-create-disabled');
       }
     };
   }
@@ -67,19 +97,22 @@ function eventCalendarDirective($injector, $parse) {
     vm.selectedEvents = [];
     vm.today = $$mdEventCalendarUtil.createDateAtMidnight();
     vm.date = $$mdEventCalendarUtil.createDateAtMidnight();
+    vm.isToday = $$mdEventCalendarUtil.isSameDay(vm.date, new Date());
     vm.monthDisplay = $$mdEventCalendarUtil.months[vm.date.getMonth()];
     vm.yearDisplay = vm.date.getFullYear();
-
+    vm.isTodayDisabled = true;
+    vm.showCreateLink = $attrs.mdShowCreateLink !== undefined && $attrs.mdShowCreateLink !== 'false';
     vm.nextMonth = nextMonth;
     vm.previousMonth = previousMonth;
     vm.selectEvent = selectEvent;
-
+    vm.setToday = setToday;
 
 
     function nextMonth() {
       vm.date = $$mdEventCalendarUtil.getDateInNextMonth(vm.date);
       vm.monthDisplay = $$mdEventCalendarUtil.months[vm.date.getMonth()];
       vm.yearDisplay = vm.date.getFullYear();
+      vm.isTodayDisabled = vm.date.getMonth() === (new Date()).getMonth();
     }
 
 
@@ -87,6 +120,14 @@ function eventCalendarDirective($injector, $parse) {
       vm.date = $$mdEventCalendarUtil.getDateInPreviousMonth(vm.date);
       vm.monthDisplay = $$mdEventCalendarUtil.months[vm.date.getMonth()];
       vm.yearDisplay = vm.date.getFullYear();
+      vm.isTodayDisabled = vm.date.getMonth() === (new Date()).getMonth();
+    }
+
+    function setToday() {
+      vm.date = new Date();
+      vm.monthDisplay = $$mdEventCalendarUtil.months[vm.date.getMonth()];
+      vm.yearDisplay = vm.date.getFullYear();
+      vm.isTodayDisabled = true;
     }
 
 
